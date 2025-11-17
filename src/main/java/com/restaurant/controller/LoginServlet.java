c9d149799517449eef8fb039229c57dacea1b053
Thang147429 <tranhuuthang.bd04@gmail.com>
2025-11-17 18:41:12 +0700

    Update LoginServlet.java

--- src/main/java/com/restaurant/controller/LoginServlet.java
+++ src/main/java/com/restaurant/controller/LoginServlet.java
@@ -8,21 +8,21 @@ import com.restaurant.service.ResService;
 
 public class LoginServlet extends HttpServlet {
 
-    private ResService service = new ResService();
+ 
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String username = req.getParameter("username");
         String password = req.getParameter("password");
 
-        User user = service.authenticateUser(username, password);
-
-        if (user != null) {
-            req.getSession().setAttribute("user", user);
-            resp.sendRedirect("listItems.jsp");
-        } else {
-            req.setAttribute("error", "Sai tài khoản hoặc mật khẩu!");
-            req.getRequestDispatcher("login.jsp").forward(req, resp);
-        }
-    }
+//        User user = service.authenticateUser(username, password);
+//
+//        if (user != null) {
+//            req.getSession().setAttribute("user", user);
+//            resp.sendRedirect("listItems.jsp");
+//        } else {
+//            req.setAttribute("error", "Sai tài khoản hoặc mật khẩu!");
+//            req.getRequestDispatcher("login.jsp").forward(req, resp);
+//        }
+  }
 }
