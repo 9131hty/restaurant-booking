e9c9a4cf178c46ec722f5c73c37182e238b7c5ad
9131hty <23130019@st.hcmuaf.edu.vn>
2026-01-04 11:37:36 +0700

    Refactor: Minor changes.

--- src/main/java/com/restaurant/common/enumeration/MeasurementUnit.java
+++ src/main/java/com/restaurant/common/enumeration/MeasurementUnit.java
@@ -1,5 +1,25 @@
 package com.restaurant.common.enumeration;
 
-public enum MeasurementUnit {
-    ML, L, G, KG, PORTION, PIECE, SIZE
+import com.restaurant.common.enumeration.CodeEnum;
+
+public enum MeasurementUnit implements CodeEnum {
+    ML, L, G, KG, PORTION, PIECE, SIZE;
+
+    @Override
+    public String getCode() {
+        return this.name().toLowerCase();
+    }
+
+    public String getLabel() {
+        switch (this) {
+            case ML: return "ml";
+            case L: return "l";
+            case G: return "g";
+            case KG: return "kg";
+            case PORTION: return "portion";
+            case PIECE: return "piece";
+            case SIZE: return "size";
+            default: return this.name().toLowerCase();
+        }
+    }
 }
